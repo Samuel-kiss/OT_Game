ca903f5189925243ad2f72b97ed3b8a859f42502
Samuel_kiss <kisssamuel37@gmail.com>
2025-01-19 20:30:39 +0100

    Oprava a vylepšenie kódu pre skoky

--- Player.py
+++ Player.py
@@ -1,3 +1,4 @@
+import pygame.time
 
 from Settings import *
 
@@ -7,10 +8,10 @@ class Player:
         self.moving = False
         self.jumping = False
         self.on_platform = False
-        self.double_jump = False
+        self.extra_jump = False
 
         self.walk_images = {"right": [], "left": []}
-        self.jump_images = {"right": [], "left": [],"double":[]}
+        self.jump_images = {"right": [], "left": [],"extra":[]}
         self.Load_walk_images()
         self.Load_jump_images()
 
@@ -19,20 +20,22 @@ class Player:
         self.rect = self.image.get_rect(midbottom=(WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
 
         self.image_index = 0
-        self.double_jump_index = 0
+        self.extra_jump_index = 0
         self.speed = 5
         self.fall = 0
-        self.double_jump_quantity = 0
+        self.extra_jump_quantity = 20
         self.bonus_life_quantity = 0
         self.g_force = 0.5
         self.jump = -10
+        self.animation_timer = pygame.time.get_ticks()
+        self.animation_delay = 60
 
         #Načítanie zvukov
         self.jump_sound = pygame.mixer.Sound(join("dist/Assets", "Music", "jump.mp3"))
         self.jump_sound.set_volume(0.9)
 
-        self.double_jump_sound = pygame.mixer.Sound(join("dist/Assets", "Music", "double_jump.mp3"))
-        self.double_jump_sound.set_volume(0.9)
+        self.extra_jump_sound = pygame.mixer.Sound(join("dist/Assets", "Music", "extra_jump.mp3"))
+        self.extra_jump_sound.set_volume(0.9)
 
         self.walk_sound = pygame.mixer.Sound(join("dist/Assets", "Music", "walk.mp3"))
         self.walk_sound.set_volume(0.9)
@@ -80,11 +83,6 @@ class Player:
 
     def Move(self):
         keys = pygame.key.get_pressed()
-
-        self.moving = False
-        self.jumping = False
-        self.double_jump = False
-
         if keys[pygame.K_LEFT]:
             if not self.rect.x< 0:
                 self.rect.x -= self.speed
@@ -99,16 +97,17 @@ class Player:
 
         if keys[pygame.K_SPACE]:
             if self.on_platform:
+                self.on_platform = False
                 self.jumping = True
                 self.fall = self.jump
                 self.jump_sound.play()
-                self.on_platform = False
+
             if not self.rect.y <= 0:
-                if not self.on_platform and self.fall>0 and self.double_jump_quantity > 0:
-                    self.double_jump = True
-                    self.double_jump_sound.play()
+                if not self.on_platform and self.fall>0 and self.extra_jump_quantity > 0:
+                    self.extra_jump = True
+                    self.extra_jump_sound.play()
                     self.fall = self.jump
-                    self.double_jump_quantity -= 1
+                    self.extra_jump_quantity -= 1
             else:
                 self.rect.y = 0
 
@@ -125,35 +124,48 @@ class Player:
                 self.rect.y = platform.y-20
                 self.fall = 0
                 self.on_platform = True
+                self.jumping = False
+                self.extra_jump = False
 
 
 
     def Animation(self,screen):
+        time = pygame.time.get_ticks()
         if not self.on_platform:
             if self.jumping:
-                if self.image_index < len(self.jump_images[self.direction]):
-                    self.image = self.jump_images[self.direction][self.image_index]
-                    self.image_index += 1
-                else:
-                    self.image = self.jump_images[self.direction][-1]
+                if time-self.animation_timer > self.animation_delay:
+                    if self.image_index<len(self.jump_images[self.direction]):
+                        self.image = self.jump_images[self.direction][self.image_index]
+                        self.image_index += 1
+                    else:
+                        self.image = self.jump_images[self.direction][-1]
+
+                    if self.extra_jump:
+                            if self.extra_jump_index < len(self.jump_images["extra"]):
+                                extra_jump_img = self.jump_images["extra"][self.extra_jump_index]
+                                extra_jump_rect = extra_jump_img.get_rect(
+                                    center=(self.rect.centerx, self.rect.bottom + 15))
+                                screen.blit(extra_jump_img, extra_jump_rect)
+                                self.extra_jump_index += 1
+                            else:
+                                self.extra_jump_index = 0
+                                self.extra_jump = False
+                    else:
+                        self.extra_jump = False
+                        self.extra_jump_index = 0
+                    self.animation_timer = time
             else:
                 self.image = self.jump_images[self.direction][-1]
                 self.image_index = 0
-
-            if self.double_jump:
-                if self.double_jump_index < len(self.jump_images["double"]):
-                    double_jump_img = self.jump_images["double"][self.double_jump_index]
-                    double_jump_rect = double_jump_img.get_rect(center=(self.rect.centerx,self.rect.bottom+15))
-                    screen.blit(double_jump_img,double_jump_rect)
-            else:
-                self.double_jump_index = 0
         else:
             if self.moving:
                 self.image_index = (self.image_index+1) % len(self.walk_images[self.direction])
                 self.image = self.walk_images[self.direction][self.image_index]
+                self.moving = False
             else:
                 self.image = self.walk_images[self.direction][0]
                 self.image_index = 0
+
         screen.blit(self.image, self.rect.topleft)
 
 
